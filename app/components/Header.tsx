b90096e7621a3d93c5825d72665d2eebb3f6ce71
Abhishek Prabhakar <abhishek@nettoraid.com>
2023-09-14 00:45:11 +0530

    header responsive

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,26 +7,36 @@ const logoStyle: React.CSSProperties = { fontSize: '20px', textTransform: 'upper
 
 export function Header() {
     return <>
-        <div style={{ borderBottom: '1px solid var(--ui-color-black)', padding: '20px 40px' }} >
-            <div className="container header-top-menu">
-                <div className="header-search">
-                    <Input placeholder="Search" bordered={false} prefix={<SearchOutlined />} />
-                </div>
-                <div>
-                    <Link to="/" style={logoStyle}>CELEBRIA COLLECTIONS</Link>
-                </div>
-                <div className="header-actions">
-                    <Button type="text" icon={<SmileOutlined />}>Login</Button>
-                    <Button icon={<ShoppingCartOutlined />} href="/checkout" />
-                </div>
+        <div style={{ borderBottom: '1px solid var(--ui-color-black)', padding: '20px 0' }} >
+            <div className="container">
+                <Row >
+                    <Col span={0} md={8} lg={4}>
+                        <Input placeholder="Search" bordered={false} prefix={<SearchOutlined />} />
+                    </Col>
+                    <Col flex={'auto'} className="header-brand">
+                        <Link to="/" style={logoStyle}>CELEBRIA COLLECTIONS</Link>
+                    </Col>
+                    <Col span={0} md={8} lg={4}>
+                        <Row gutter={[0, 10]} justify={'end'}>
+                            <Col>
+                                <Button type="text" icon={<SmileOutlined />}>Login</Button>
+                            </Col>
+                            <Col>
+                                <Button icon={<ShoppingCartOutlined />} href="/checkout" />
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
             </div>
         </div>
-        <div style={{ borderBottom: '1px solid var(--ui-color-black)', padding: '0px 40px' }} >
-            <Row justify={'center'}>
-                <Col>
-                    <AppNavigation />
-                </Col>
-            </Row>
-        </div>
+        <Row>
+            <Col span={0} md={24} style={{ borderBottom: '1px solid var(--ui-color-black)', padding: '0px 40px' }} >
+                <Row justify={'center'}>
+                    <Col>
+                        <AppNavigation />
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
     </>
 }
