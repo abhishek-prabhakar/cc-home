ef996f8698dd66565fdc0366b3e20964992c885f
Abhishek Prabhakar <abhishek@nettoraid.com>
2023-10-19 22:47:45 +0530

    page loader

--- app/root.tsx
+++ app/root.tsx
@@ -13,7 +13,7 @@ import cssTransitions from '~/transitions.css';
 import styles from '~/root.css';
 import antdStyles from '~/antd.css';
 import carouselStyles from 'react-photo-view/dist/react-photo-view.css';
-import { Layout } from "antd";
+import { Col, Layout, Row, Spin } from "antd";
 import { Footer } from "~/components/Footer";
 import { Ticker } from "~/components/Ticker";
 import { Header } from "./components/Header";
@@ -23,6 +23,7 @@ import { db } from "./utils/database";
 const { Content } = Layout;
 import { Provider } from 'react-redux';
 import store from './store/store';
+import { useEffect, useState } from "react";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
@@ -62,6 +63,12 @@ export async function loader({ request }: LoaderArgs): Promise<any | User | null
 }
 
 export default function App() {
+  const [pageReady, setPageReady] = useState(false);
+
+  useEffect(() => {
+    setPageReady(true);
+  }, []);
+
 
   return (
     <html lang="en">
@@ -78,8 +85,12 @@ export default function App() {
             <Layout.Header style={headerStyle}>
               <Header />
             </Layout.Header>
-            <Content style={{ paddingTop: '40px', overflow: 'hidden' }}>
-              <Outlet />
+            <Content style={{ paddingTop: '40px' }}>
+              {pageReady ? <Outlet /> : <Row justify={'center'}>
+                <Col>
+                  <Spin />
+                </Col>
+              </Row>}
             </Content>
             <Layout.Footer style={{ background: 'none', padding: '24px 20px' }}>
               <Footer />
