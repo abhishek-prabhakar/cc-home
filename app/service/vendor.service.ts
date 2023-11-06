e69f0710ca327bdfb3cf270d83c458eeba63496c
Abhishek Prabhakar <abhishek@nettoraid.com>
2023-11-07 01:27:40 +0530

    sort result

--- app/service/vendor.service.ts
+++ app/service/vendor.service.ts
@@ -85,6 +85,9 @@ export const VendorQuery = {
                             }
                         }
                     }
+                },
+                orderBy: {
+                    name: 'asc'
                 }
             }).then(r => {
                 resolve(r.map(x => ({
