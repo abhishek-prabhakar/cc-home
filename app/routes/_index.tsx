f374ffd8a6a88229b42386a8e62ea77feb09f04f
Abhishek Prabhakar <abhishek@nettoraid.com>
2023-12-06 21:17:20 +0530

    slider style

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -53,22 +53,12 @@ export async function loader({ params }: LoaderArgs): Promise<TypedDeferredData<
     label: 'Photography',
     title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    img: 'https://demo.craftdzine.com/html/xberg/assets/img/hero-bg1.png'
+    img: 'https://www.bollywoodshaadis.com/img-scale/640/article-2015822113182847908000.jpg'
   }, {
     label: 'Photography',
     title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    img: 'https://demo.craftdzine.com/html/xberg/assets/img/hero-bg1.png'
-  }, {
-    label: 'Photography',
-    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    img: 'https://demo.craftdzine.com/html/xberg/assets/img/hero-bg1.png'
-  }, {
-    label: 'Photography',
-    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    img: 'https://demo.craftdzine.com/html/xberg/assets/img/hero-bg1.png'
+    img: 'https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/k-53-teddy-3307_0.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=e92a96a06ad219e23a637e8a2e784b78'
   }];
 
   const quickLinks = new Promise<Collection[]>(function (resolve) {
@@ -172,7 +162,7 @@ export const meta: V2_MetaFunction = () => {
 const jumbotronWrapperStyle = { overflow: 'auto', };
 const jumbotronListStyle = { display: 'flex', gap: '20px', alignItems: 'center' }
 const jumbotronItemWrapperStyle: React.CSSProperties = { width: '60vw' }
-const jumbotronItemStyle: React.CSSProperties = { padding: '50px 20px', width: '100%', borderRadius: '15px', background: 'url(https://demo.craftdzine.com/html/xberg/assets/img/hero-bg1.png) center no-repeat #c0c0c0' }
+const jumbotronItemStyle: React.CSSProperties = { padding: '50px 20px', width: '100%', borderRadius: '15px', }
 
 const FALLBACK_IMG = 'https://static.miraheze.org/widdershinswiki/thumb/4/47/Placeholder.png/800px-Placeholder.png';
 
@@ -206,24 +196,28 @@ const Home = {
     const [active, setActive] = useState(1);
 
     return <div className="home-slider-jumbotron"><Carousel>
-      {data.jumbotron.map((item, key) => <div key={'slider-' + key} style={{ padding: '0 20px' }}>
-        <Row style={jumbotronItemStyle} justify={'center'}>
-          <Col xs={24} sm={24} md={20}>
-            <Row gutter={[40, 40]} align={'bottom'}>
-              <Col span={24}>
-                <Title style={{ color: 'white' }} level={2}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                </Title>
-              </Col>
-              <Col xs={24} sm={24} md={18} lg={16} xl={12}>
-                <Card bordered={false}>
-                  <p><Tag color="cyan">Photography</Tag></p>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                </Card>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
+      {data.jumbotron.map((item, key) => <div key={'slider-' + key}>
+        <div style={{ padding: '0 20px' }}>
+          <Row justify={'center'}>
+            <Col xs={24} sm={24} md={20} lg={18}>
+              <div style={{ ...jumbotronItemStyle, background: `url(${item.img}) center no-repeat #c0c0c0` }}>
+                <Row gutter={[40, 40]} align={'bottom'}>
+                  <Col span={24}>
+                    <Title style={{ color: 'white' }} level={2}>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                    </Title>
+                  </Col>
+                  <Col xs={24} sm={24} md={18} lg={16} xl={12}>
+                    <Card bordered={false}>
+                      <p><Tag color="cyan">Photography</Tag></p>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </Card>
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        </div>
       </div>)}
     </Carousel></div>;
   },
