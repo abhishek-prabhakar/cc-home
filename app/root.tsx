f7fd9125582c03e35c3c48993065d9a3ef30e291
Abhishek Prabhakar <abhishek@nettoraid.com>
2023-12-20 14:29:20 +0530

    debug index body

--- app/root.tsx
+++ app/root.tsx
@@ -116,7 +116,18 @@ export default function App() {
                 {response => <Header user={response} />}
               </Await>
             </Layout.Header>
-
+            <Content style={{ paddingTop: '40px' }}>
+              {navigation.state === 'idle' || navigation.state === 'submitting' ? <Outlet /> : <div className="container"><Row gutter={[80, 80]}>
+                <Col xs={24} sm={24} md={16}>
+                  <Skeleton />
+                  <Skeleton />
+                  <Skeleton />
+                </Col>
+                <Col xs={24} sm={24} md={8}>
+                  <Skeleton />
+                </Col>
+              </Row></div>}
+            </Content>
             <Layout.Footer style={{ background: 'none', padding: '24px 20px' }}>
               <Footer />
             </Layout.Footer>
