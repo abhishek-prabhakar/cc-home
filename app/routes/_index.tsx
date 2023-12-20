1562d27e3bd99218bfca22f37f5ff6bcaacfca94
Abhishek Prabhakar <abhishek@nettoraid.com>
2023-12-20 14:52:42 +0530

    debug enabled section

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -293,14 +293,14 @@ const Home = {
 
     return (
       <div >
-        {/* <Home.Jumbotron /> */}
+        <Home.Jumbotron />
         <div className="container">
           <Row gutter={[0, 50]}>
             <Col span={24}>
-              {/* <Home.Services /> */}
+              <Home.Services />
             </Col>
             <Col span={24}>
-              {/* <Home.QuickPick /> */}
+              <Home.QuickPick />
             </Col>
             <Col span={24}>
               {/* <Await resolve={data.bannerAds}>
