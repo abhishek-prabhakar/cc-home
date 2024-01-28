cd7377d022afd1832da20561642945ee10dde0e3
Abhishek Prabhakar <abhishek@nettoraid.com>
2024-01-28 21:39:39 +0530

    debug

--- app/root.tsx
+++ app/root.tsx
@@ -154,11 +154,11 @@ export default function App() {
           <Layout>
             <Ticker />
             <Layout.Header style={headerStyle}>
-              <Suspense fallback={<Skeleton />}>
+              {/* <Suspense fallback={<Skeleton />}>
                 <Await resolve={data.user}>
                   {response => <Header user={response} />}
                 </Await>
-              </Suspense>
+              </Suspense> */}
             </Layout.Header>
             <Content style={{ paddingTop: '40px' }}>
               {navigation.state === 'idle' || navigation.state === 'submitting' ? <Outlet /> : <div className="container"><Row gutter={[80, 80]}>
