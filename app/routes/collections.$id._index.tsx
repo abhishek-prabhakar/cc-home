8c1c580b43f4102dd8f00f4b397af0432fbf42f4
Abhishek Prabhakar <abhishek@nettoraid.com>
2024-01-31 20:33:32 +0530

    card style

--- app/routes/collections.$id._index.tsx
+++ app/routes/collections.$id._index.tsx
@@ -72,7 +72,7 @@ const CollectionsPage = {
                             key: item.keyName,
                             label: <Typography.Title level={4}>{item.name}</Typography.Title>,
                             children: <Row key={item.keyName} gutter={[40, 40]} style={{ padding: '40px 0' }}>
-                                {item.serviceGroup.map(service => <Col key={service.id} sm={12} xs={12} md={6}><Link to={Routes.ServiceGroup.replace(':id', item.keyName || '').replace(':subId', service.id)}>
+                                {item.serviceGroup.map(service => <Col key={service.id} sm={24} xs={24} md={6} style={{ padding: '20px 0' }}><Link to={Routes.ServiceGroup.replace(':id', item.keyName || '').replace(':subId', service.id)}>
                                     <div style={{ borderRadius: '10px', background: '#F5F5F7', padding: '12px', boxShadow: '0 2px 4px #d3d3d3' }}>
                                         <Image preview={false} src={service.imageName ? PATH.RESOURCE_URL + service.imageName : ''} style={{ borderRadius: '6px', maxHeight: '140px', objectFit: 'cover' }} width={'100%'} fallback={PATH.FALLBACK_IMG} />
                                         <div style={{ paddingBottom: '20px' }}></div>
