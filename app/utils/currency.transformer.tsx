fbc21debcdd7c26425e79503705626d388b4fff5
Abhishek Prabhakar <abhishek@nettoraid.com>
2024-03-06 19:41:07 +0530

    round figures

--- app/utils/currency.transformer.tsx
+++ app/utils/currency.transformer.tsx
@@ -2,7 +2,7 @@ import { NumberFormatter } from "@mantine/core";
 import COMMON_DATA from "~/data/common.data";
 
 function Currency({ value }: { value?: number }) {
-    return <NumberFormatter prefix={COMMON_DATA.currency} value={value || 0} thousandSeparator />;
+    return <NumberFormatter prefix={COMMON_DATA.currency} value={Math.ceil(value || 0)} thousandSeparator />;
 }
 
 export default Currency;
