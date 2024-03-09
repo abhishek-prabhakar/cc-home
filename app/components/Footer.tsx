b5bc58db7986b807ee68d481ad488e999436462b
Abhishek Prabhakar <abhishek@nettoraid.com>
2024-03-10 00:30:04 +0530

    footer ui

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,34 +8,35 @@ import Skeleton from "./Skeleton";
 export function Footer() {
     const data = useLoaderData<RootLoaderData>();
 
-    return <Container size={'xl'} pt={'xl'}>
-        <Space h="xl" />
-        <Link to="/" ><img src="/assets/brand-logo-1.png" width={'124px'} /></Link>
-        <Space h="lg" />
-        <Suspense fallback={<Skeleton />}>
-            <Await resolve={data.pages}>
-                {navList =>
-                    <Grid gutter={'lg'}>
-                        {navList.map(item => <Grid.Col span="content">
-                            <Text fw={500} pb={'sm'}>{item.name}</Text>
-                            <Stack>
-                                {item.children[0]?.list.map((child, i) => <Link to={child.path}><Text size="sm">{child.name}</Text></Link>)}
-                            </Stack>
-                        </Grid.Col>)}
-                    </Grid>
-                }
-            </Await>
-        </Suspense>
-        <Space h="xl" />
-        <Divider />
-        <Space h="xl" />
-        <Text ta="center">© Celebria Collective 2024. All Rights Reserved.</Text>
-        <Space h="md" />
-        <Group justify="center" gap="lg">
-            <Text c="dimmed" ta="center">© Celebria Collective 2024. All Rights Reserved.</Text>
-            <Text c="dimmed" ta="center">Privacy Policy</Text>
-            <Text c="dimmed" ta="center">Terms And Conditions</Text>
-        </Group>
-        <Space h="xl" />
-    </Container>;
+    return <div style={{ background: '#fbfbfb', marginTop: '100px' }}>
+        <Container size={'lg'} pt={'xl'}>
+            <Space h="xl" />
+            <Link to="/" ><img src="/assets/brand-logo-1.png" width={'124px'} /></Link>
+            <Space h="lg" />
+            <Suspense fallback={<Skeleton />}>
+                <Await resolve={data.pages}>
+                    {navList =>
+                        <Grid gutter={'xl'}>
+                            {navList.map(item => <Grid.Col span="content">
+                                <Text fw={500} pb={'sm'}>{item.name}</Text>
+                                <Stack>
+                                    {item.children[0]?.list.map((child, i) => <Link to={child.path}><Text size="sm">{child.name}</Text></Link>)}
+                                </Stack>
+                            </Grid.Col>)}
+                        </Grid>
+                    }
+                </Await>
+            </Suspense>
+            <Space h="xl" />
+            <Divider />
+            <Space h="xl" />
+            <Text ta="center">© Celebria Collective 2024. All Rights Reserved.</Text>
+            <Space h="md" />
+            <Group justify="center" gap="lg">
+                <Text c="dimmed" ta="center">Privacy Policy</Text>
+                <Text c="dimmed" ta="center">Terms And Conditions</Text>
+            </Group>
+            <Space h="xl" />
+        </Container>
+    </div>;
 }
