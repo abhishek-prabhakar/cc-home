bc0e13322ac909b80b252408113a40b5c37a1b6c
Abhishek Prabhakar <abhishek@nettoraid.com>
2024-03-11 18:41:11 +0530

    cat input

--- app/routes/cart.checkout._index.tsx
+++ app/routes/cart.checkout._index.tsx
@@ -51,7 +51,7 @@ const Cart = {
                                 <Stack gap={'md'}>
                                     <Cart.Summary data={response} />
                                     {user ? <Form method="post" action="/order/checkout">
-                                        <input type="hidde" name="cart" value={data.cartInput} />
+                                        <input type="hidden" name="cart" value={data.cartInput} />
                                         <Button variant="filled" type="submit" radius={'xl'}>Place order</Button></Form> : <UserLogin title="Login to continue" redirectUrl="/cart/checkout" />}
                                 </Stack>
                             }
