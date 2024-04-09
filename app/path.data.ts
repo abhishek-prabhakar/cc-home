0b8289bd88f2dea46e5859eb6866c84c5b1d4707
Abhishek Prabhakar <abhishek@nettoraid.com>
2024-04-09 19:33:55 +0530

    path

--- app/path.data.ts
+++ app/path.data.ts
@@ -3,7 +3,7 @@ export const PATH = {
     FALLBACK_IMG: 'https://static.miraheze.org/widdershinswiki/thumb/4/47/Placeholder.png/800px-Placeholder.png',
     AVATAR_PLACEHOLDER: 'https://xsgames.co/randomusers/avatar.php?g=pixel',
     FILE_UPLOAD: 'https://asset.hotmommy.in/asset/upload',
-    GUEST_FILE_UPLOAD: "https://asset.hotmommy.in/guest/upload/",
+    GUEST_FILE_UPLOAD: "https://asset.hotmommy.in/guest/upload",
     // FILE_UPLOAD: "http://localhost:3003/upload/",
     // GUEST_FILE_UPLOAD: "http://localhost:3003/guest/upload/",
 }
