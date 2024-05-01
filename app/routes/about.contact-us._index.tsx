973cf1fca440bcd9b4314026d7c95c97cd576eae
Abhishek Prabhakar <abhishek@nettoraid.com>
2024-05-01 15:14:20 +0530

    og data update

--- app/routes/about.contact-us._index.tsx
+++ app/routes/about.contact-us._index.tsx
@@ -11,23 +11,23 @@ export default function () {
             <Grid.Col span={{ base: 12, md: 4 }}>
                 <List spacing="lg">
                     <List.Item icon={
-                        <ThemeIcon color="blue" size={24} variant="white">
+                        <ThemeIcon color="gray" size={24} variant="white">
                             <IconPhone style={{ width: rem(16), height: rem(16) }} />
                         </ThemeIcon>
                     }>+91-6363369715</List.Item>
                     <List.Item icon={
-                        <ThemeIcon color="blue" size={24} variant="white">
+                        <ThemeIcon color="gray" size={24} variant="white">
                             <IconMail style={{ width: rem(16), height: rem(16) }} />
                         </ThemeIcon>
                     }>info@celebriacollective.com</List.Item>
                     <List.Item icon={
-                        <ThemeIcon color="blue" size={24} variant="white">
+                        <ThemeIcon color="gray" size={24} variant="white">
                             <IconMapPin style={{ width: rem(16), height: rem(16) }} />
                         </ThemeIcon>
                     }>Celebria Collective Private Limited: 02,
                         VASANTHA TOWERS, COCONUT garden layout, Krishnarajapuram, Bangalore North, Bangalore, Karnataka - 560036</List.Item>
                     <List.Item icon={
-                        <ThemeIcon color="blue" size={24} variant="white">
+                        <ThemeIcon color="gray" size={24} variant="white">
                             <IconBrandWhatsapp style={{ width: rem(16), height: rem(16) }} />
                         </ThemeIcon>
                     }><a href="https://wa.me/6363369715">chat on whatsapp</a></List.Item>
