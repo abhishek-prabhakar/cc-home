8ab592b41fdd03c06355a28a9843242507537d41
Abhishek Prabhakar <abhishek@nettoraid.com>
2024-05-06 12:38:11 +0530

    ticker data

--- app/components/Ticker.tsx
+++ app/components/Ticker.tsx
@@ -1,6 +1,6 @@
 export function Ticker() {
 
     return <div style={{ background: 'black', color: '#d8d8d8', fontSize: '10px', padding: '5px 10px', textAlign: 'center' }}>
-        USE CODE NEW30 for 30%
+        Use code WELCOME40 to get 40% discount on your first order.
     </div>
 }
