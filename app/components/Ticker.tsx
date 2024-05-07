bd172b57408772bc0a2cfa8b2b640ff91ce075a9
Abhishek Prabhakar <abhishek@nettoraid.com>
2024-05-07 18:40:08 +0530

    coupond ata

--- app/components/Ticker.tsx
+++ app/components/Ticker.tsx
@@ -1,6 +1,6 @@
 export function Ticker() {
 
     return <div style={{ background: 'black', color: '#d8d8d8', fontSize: '10px', padding: '5px 10px', textAlign: 'center' }}>
-        Use code WELCOME40 to get 40% discount on your first order.
+        Use code WELCOME40 to get 40% discount.
     </div>
 }
