41851b1f5e6a1ecafe4cc64636ca763d2e2354c7
Abhishek Prabhakar <hello@makewithabhishek.com>
2024-07-02 10:54:33 +0530

    vendor affix

--- app/components/ChatWithVendorAffix.tsx
+++ app/components/ChatWithVendorAffix.tsx
@@ -13,7 +13,7 @@ export function ChatWithVendorAffix(p:{
             {(transitionStyles) => (
               <Card style={{...transitionStyles, overflow: 'unset'}} radius={'lg'} withBorder shadow='xl' pos="relative">
                     <CloseButton  size="sm" onClick={()=>setShowCard(false)} pos="absolute" right={'12px'} top={'12px'} bg="#e1e1e1"/>
-                    <Avatar  pos="absolute" size={'lg'} src={p.avatar} right={'-14px'} bottom={'-14px'}/>
+                    <Avatar  pos="absolute" size={'lg'} src={p.avatar} right={'-8px'} bottom={'-8px'}/>
                     <Title order={5}>Got any queries?</Title>
                     <Space h="sm"/>
                     <Text>Get start with a service to chat with me.</Text>
