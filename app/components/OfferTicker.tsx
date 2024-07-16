069be87d945be8d683046ea0fd3c6fb0c61d1fc7
Abhishek Prabhakar <hello@makewithabhishek.com>
2024-07-16 12:54:13 +0530

    typo fix

--- app/components/OfferTicker.tsx
+++ app/components/OfferTicker.tsx
@@ -1,7 +1,7 @@
 export function OfferTicker() {
-    const coupon = 'CELEB40';
+    const coupon = 'NEW30';
 
     return <div style={{ background: 'black', color: '#d8d8d8', fontSize: '10px', padding: '5px 10px', textAlign: 'center' }}>
-        Use code {coupon} to get 40% discount.
+        Use code {coupon} to get 30% discount.
     </div>
 }
--- app/routes/_auth.vendor.manage.$orderId._index.tsx
+++ app/routes/_auth.vendor.manage.$orderId._index.tsx
@@ -1,6 +1,60 @@
 import { Container } from "@mantine/core";
+import { LoaderArgs } from "@remix-run/node";
+import { USER_SESSION_KEY, getSession } from "~/session.server";
+import { db } from "~/utils/database";
+
+
+
+export async function loader(args: LoaderArgs){
+    const session = await getSession(
+        args.request.headers.get("Cookie")
+    );
+    const userId = session.get(USER_SESSION_KEY);
+    const orderId = args.params.orderId;
+
+    if (!userId) {
+        throw new Error('Page not found');
+    }
+
+    const user = await db.user.findFirstOrThrow({
+        select:{
+            username: true
+        },
+        where:{
+            id: userId
+        }
+    });
+
+    const vendor =  await db.vendor.findFirstOrThrow({
+        where:{
+            mobileNumber: user.username
+        },
+        select:{
+            id: true
+        }
+    });
+
+    console.log(orderId)
+
+    const orderData =  await db.bookingService.findFirstOrThrow({
+        where:{
+            Booking:{
+                orderId
+            },
+            vendorServiceGroup:{
+                vendorId: vendor.id
+            }
+        },
+        select:{
+            id: true
+        }
+    });
+
+    return orderData;
+}
 
 export default function(){
+
     return <Container size={'xl'}>
         sdfs
     </Container>
