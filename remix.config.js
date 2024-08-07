c9b5105db6196c3d07031f9b2de6f483b4f3a483
Abhishek Prabhakar <hello@makewithabhishek.com>
2024-08-07 14:29:38 +0530

    code cleanup

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,10 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
   server:
   process.env.NETLIFY || process.env.NETLIFY_LOCAL
     ? "./server.ts"
     : undefined,
-  // serverBuildPath: ".netlify/functions-internal/server.js",
   serverModuleFormat: "cjs",
   postcss: true,
   future: {
