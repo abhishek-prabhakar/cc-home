d127c3f7184cf59c4db31fa70ed2f9ec5df1b69f
Abhishek Prabhakar <hello@makewithabhishek.com>
2024-08-08 17:32:17 +0530

    ticker content

--- app/components/OfferTicker.tsx
+++ app/components/OfferTicker.tsx
@@ -1,5 +1,5 @@
 export function OfferTicker() {
-    const coupon = 'NEW30';
+    const coupon = 'NEW15';
 
     return <div style={{ background: 'black', color: '#d8d8d8', fontSize: '10px', padding: '5px 10px', textAlign: 'center' }}>
         Use code {coupon} to get 30% discount.
