81c4002bd70ebfc5af7871ede6be02b0440cee50
Abhishek Prabhakar <hello@makewithabhishek.com>
2024-08-10 14:03:17 +0530

    refund policy changes

--- app/routes/about.privacy-policy._index.tsx
+++ app/routes/about.privacy-policy._index.tsx
@@ -5,10 +5,14 @@ export default function () {
     return <Container>
         <Title order={3}>Privacy Policy</Title>
         <Space h="md" />
-        <p>Last updated on May 3th 2024</p>
+        <p>Last updated on Aug 10th 2024</p>
         <p>This Privacy Policy describes how Celebria Collective ("we", "our", or "us") collects, uses, and protects the personal information you provide when using our website and services.</p>
 
-        <p>Information We Collect</p>
+        <Title order={5}>Ownership of Photos/Videos</Title>
+        <p>At Celebria Collective, we respect the intellectual property rights of our vendors. We do not claim ownership of any photos, videos, or other creative content uploaded by our vendors on the platform. All such content remains the exclusive property of the vendor who created it.
+If you are a vendor or user who believes that your work has been uploaded on our platform without your permission, or if you would like any content to be removed for any reason, please contact us at info@celebriacollective.com. We will promptly review your request and take appropriate action, including the removal of the content if necessary.</p>
+
+<Title order={5}>Information We Collect</Title>
 
         <ul>
             <li>Personal Information: When you register on our platform, we may collect personal information such as your name, email address, phone number, and location.</li>
@@ -18,19 +22,19 @@ export default function () {
             <li>Usage Data: We gather information about how you use our website and services, including browsing history, IP address, and device information.</li>
         </ul>
 
-        <p>How We Use Your Information</p>
+        <Title order={5}>How We Use Your Information</Title>
 
         <li>Service Provision: We use your personal information to provide our services, including facilitating bookings, managing accounts, and processing payments.</li >
         <li>Communication: We may use your contact information to communicate with you about bookings, promotions, and updates related to Celebria Collective.</li >
         <li>Improvement of Services: Your feedback and usage data help us improve our platform, services, and user experience.</li >
         <li>Marketing: With your consent, we may send promotional emails or messages about new features, offers, or events.</li >
 
-
-        <p>Data Security</p>
+<br/>
+        <Title order={5}>Data Security</Title>
 
         <p>We are committed to ensuring the security of your personal information and have implemented appropriate measures to protect it from unauthorized access, misuse, or alteration.</p>
 
-        <p>Sharing of Information</p>
+        <Title order={5}>Sharing of Information</Title>
 
         <p>We do not sell, trade, or rent your personal information to third parties.However, we may share your information with:</p >
         <ul>
@@ -38,19 +42,19 @@ export default function () {
             <li>Service Providers: We may share data with trusted third-party service providers who assist us in operating our platform and providing services.</li>
         </ul>
 
-        <p>Your Choices</p>
+        <Title order={5}>Your Choices</Title>
         <ul>
             <li>Opt - Out: You may opt out of receiving promotional communications from us by following the unsubscribe instructions included in our emails or contacting us directly.</li >
             <li>Access and Update: You have the right to access and update your personal information stored on our platform.You can edit your profile information and booking details through your account settings.</li >
             <li>Delete Account: If you wish to delete your Celebria Collective account, please contact us, and we will assist you in the process.</li>
         </ul>
-        <p>Changes to This Policy</p>
+        <Title order={5}>Changes to This Policy</Title>
 
         <p>We may update this Privacy Policy from time to time to reflect changes in our practices or legal requirements.We encourage you to review this page periodically for any updates.</p>
 
-        <p>Contact Us</p>
+        <Title order={5}>Contact Us</Title>
 
-        <p>If you have any questions or concerns about our Privacy Policy, please contact us at info @celebriacollective.com.</p>
+        <p>If you have any questions or concerns about our Privacy Policy, please contact us at info@celebriacollective.com.</p>
     </Container >
 
 }
