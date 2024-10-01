b424995dff39f033e9470cb82b0369cf0aa8d96c
Abhishek Prabhakar <hello@makewithabhishek.com>
2024-10-01 23:01:15 +0530

    share ui

--- app/components/ShareOptions.tsx
+++ app/components/ShareOptions.tsx
@@ -1,5 +1,6 @@
-import { Button, CopyButton, Group } from "@mantine/core";
-import { useLocation, useNavigation } from "@remix-run/react";
+import { Box, Button, CopyButton, Group, Popover, Space, Text, Title, rem } from "@mantine/core";
+import { useLocation } from "@remix-run/react";
+import { IconShare, IconShare3 } from "@tabler/icons-react";
 import { useEffect, useState } from "react";
 import {
     EmailShareButton,
@@ -26,33 +27,45 @@ function ShareOptions(){
     },[location.pathname]);
 
     const size = '24px';
-    return <Group gap={'sm'} justify="center">
-        <CopyButton value={url}>
-            {({ copied, copy }) => (
-            <Button h={size} size="xs" variant="outline" color={copied ? 'teal' : 'violet'} onClick={copy}>
-                {copied ? 'Copied url' : 'Copy url'}
-            </Button>
-        )}
-        </CopyButton>
-        <WhatsappShareButton style={{height: size}}  url={url}>
-            <WhatsappIcon size={size}/>
-        </WhatsappShareButton>
-        {/* <FacebookMessengerShareButton  url={"http://cc.com"}>
-            <FacebookMessengerIcon size={style}/>
-        </FacebookMessengerShareButton> */}
-        <FacebookShareButton  style={{height: size}}  url={url}>
-            <FacebookIcon size={size}/>
-        </FacebookShareButton>
-        <EmailShareButton  style={{height: size}}  url={url}>
-            <EmailIcon size={size}/>
-        </EmailShareButton>
-        <TelegramShareButton  style={{height: size}}  url={url}>
-            <TelegramIcon size={size}/>
-        </TelegramShareButton>
-        <TwitterShareButton  style={{height: size}}  url={url}>
-            <TwitterIcon size={size}/>
-        </TwitterShareButton>
-    </Group>
+    return  <Popover width={204} position="bottom" withArrow shadow="md">
+    <Popover.Target>
+        <Button fullWidth variant="light"  color="violet" rightSection={<IconShare3 style={{ width: rem(20), height: rem(20) }}/>}>Share</Button>
+    </Popover.Target>
+    <Popover.Dropdown>
+        <Box>
+            <Text fw={'bold'}>Share on:</Text>
+            <Space h={'sm'}/>
+            <Group gap={'sm'}>
+                <WhatsappShareButton style={{height: size}}  url={url}>
+                    <WhatsappIcon size={size}/>
+                </WhatsappShareButton>
+                {/* <FacebookMessengerShareButton  url={"http://cc.com"}>
+                    <FacebookMessengerIcon size={style}/>
+                </FacebookMessengerShareButton> */}
+                <FacebookShareButton  style={{height: size}}  url={url}>
+                    <FacebookIcon size={size}/>
+                </FacebookShareButton>
+                <EmailShareButton  style={{height: size}}  url={url}>
+                    <EmailIcon size={size}/>
+                </EmailShareButton>
+                <TelegramShareButton  style={{height: size}}  url={url}>
+                    <TelegramIcon size={size}/>
+                </TelegramShareButton>
+                <TwitterShareButton  style={{height: size}}  url={url}>
+                    <TwitterIcon size={size}/>
+                </TwitterShareButton>
+            </Group>
+            <Space h={'md'}/>
+            <CopyButton value={url}>
+                {({ copied, copy }) => (
+                    <Button size="sm" fullWidth variant="light" color={copied ? 'teal' : 'indigo'} onClick={copy}>
+                        {copied ? 'Copied url' : 'Copy url'}
+                    </Button>
+                )}
+            </CopyButton>
+        </Box>
+    </Popover.Dropdown>
+  </Popover>;
 }
 
 export default ShareOptions;
