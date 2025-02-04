d3389413cc0fa4d4844fc7322ca1dcf69b666cf1
Abhishhek <hello@makewithabhishek.com>
2025-02-04 18:07:43 +0530

    updated stories data

--- app/contents/home-page-stories.ts
+++ app/contents/home-page-stories.ts
@@ -5,25 +5,25 @@ const REMOTE_URL = PATH.GENERAL_ASSETS+'/videos/';
 const homePageStoryAlbums: StoryAlbum[] = [
     {
       id: 'album1',
-      filePath: REMOTE_URL+'cover-2.mp4',
+      filePath: REMOTE_URL+'cover-2a.mp4',
       name: 'Testimonial',
       type: 'video'
     },
     {
       id: 'album2',
-      filePath: REMOTE_URL+'cover-3.mp4',
+      filePath: REMOTE_URL+'cover-3a.mp4',
       name: 'AD/Product',
       type: 'video'
     },
     {
       id: 'album3',
-      filePath: REMOTE_URL+'cover-1.mp4',
+      filePath: REMOTE_URL+'cover-1a.mp4',
       name: 'MUA',
       type: 'video'
     },
     {
       id: 'album4',
-      filePath: REMOTE_URL+'cover-4.mp4',
+      filePath: REMOTE_URL+'cover-4a.mp4',
       name: 'Wedding',
       type: 'video'
     }];
@@ -31,88 +31,93 @@ const homePageStoryAlbums: StoryAlbum[] = [
     const homePageStories:{ [key in string] : Story[]} = {
         album1: [
             {
-                url: REMOTE_URL+'t1.mp4',
+                url: REMOTE_URL+'t1a.mp4',
                 type:'video',
                 preloadResource: true
             },
             {
-                url: REMOTE_URL+'t2.mp4',
+                url: REMOTE_URL+'t2a.mp4',
+                type:'video',
+                preloadResource: true
+            },
+            {
+                url: REMOTE_URL+'t3a.mp4',
                 type:'video',
                 preloadResource: true
             }
         ],
         album2: [
             {
-                url: REMOTE_URL+'p3.mp4',
+                url: REMOTE_URL+'p1a.mp4',
                 type:'video',
                 preloadResource: true
             },
             {
-                url: REMOTE_URL+'p1.mp4',
+                url: REMOTE_URL+'p3a.mp4',
                 type:'video',
                 preloadResource: true
             },
             {
-                url: REMOTE_URL+'p2.mp4',
+                url: REMOTE_URL+'p2a.mp4',
                 type:'video',
                 preloadResource: true
             },
             {
-                url: REMOTE_URL+'p4.mp4',
+                url: REMOTE_URL+'p4a.mp4',
                 type:'video',
                 preloadResource: true
             }
         ],
         album3: [
             {
-                url: REMOTE_URL+'m1.mp4',
+                url: REMOTE_URL+'m1a.mp4',
                 type:'video',
                 preloadResource: true
             },
             {
-                url: REMOTE_URL+'m2.mp4',
+                url: REMOTE_URL+'m2a.mp4',
                 type:'video',
                 preloadResource: true
             }
         ],
         album4: [
             {
-                url: REMOTE_URL+'w1.mp4',
+                url: REMOTE_URL+'w1a.mp4',
                 type:'video',
                 preloadResource: true
             },
             {
-                url: REMOTE_URL+'w2.mp4',
+                url: REMOTE_URL+'w2a.mp4',
                 type:'video',
                 preloadResource: true
             },
             {
-                url: REMOTE_URL+'w3.mp4',
+                url: REMOTE_URL+'w3a.mp4',
                 type:'video',
                 preloadResource: true
             },
             {
-                url: REMOTE_URL+'w4.mp4',
+                url: REMOTE_URL+'w4a.mp4',
                 type:'video',
                 preloadResource: true
             },
             {
-                url: REMOTE_URL+'w5.mp4',
+                url: REMOTE_URL+'w5a.mp4',
                 type:'video',
                 preloadResource: true
             },
             {
-                url: REMOTE_URL+'w6.mp4',
+                url: REMOTE_URL+'w6a.mp4',
                 type:'video',
                 preloadResource: true
             },
             {
-                url: REMOTE_URL+'w7.mp4',
+                url: REMOTE_URL+'w7a.mp4',
                 type:'video',
                 preloadResource: true
             },
             {
-                url: REMOTE_URL+'w8.mp4',
+                url: REMOTE_URL+'w8a.mp4',
                 type:'video',
                 preloadResource: true
             }
